46dde789869f534f7d3ae74c054af4d4f6e67ff5
Andre Fernando Rua <andre.rua@hitachivantara.com>
2018-12-19 15:40:34 +0000

    SImples hoc (Higher Order component) aux or using Fragments

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 import classes from "./Cockpit.css";
+import Aux from "../../hoc/Aux";
 
 const cockpit = (props) => {
     const assignedClasses = [];
-    let btnClass = "";
+    let btnClass = classes.Button;
 
     if (props.showPersons) {
-        btnClass = classes.red;
+        btnClass = [classes.Button, classes.red].join(" ");
     }
 
     if (props.persons.length <= 2) {
@@ -18,14 +19,23 @@ const cockpit = (props) => {
     }
 
     return (
-        <div className={classes.Cockpit}>
+        <Aux>
             <h1>{props.appTitle}</h1>
             {/* This way of calling a method is not recommended because it can be ineficient, however it can be used if needed */}
             <p className={assignedClasses.join(" ")}>This is really working :D</p>
             <button
                 className={btnClass}
                 onClick={props.clicked}>Toggle persons</button>
-        </div>
+        </Aux>
+        // NOTE: With React 16.2 we can use Fragment like below, that does the same as our Aux component did
+        // <>
+        //     <h1>{props.appTitle}</h1>
+        //     {/* This way of calling a method is not recommended because it can be ineficient, however it can be used if needed */}
+        //     <p className={assignedClasses.join(" ")}>This is really working :D</p>
+        //     <button
+        //         className={btnClass}
+        //         onClick={props.clicked}>Toggle persons</button>
+        // </>
     );
 };
 
